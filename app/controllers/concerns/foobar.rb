1894448d388c830cb6a697ff2c3c2be20fde0f8c
Vishnu <89022.ayyappan@students.itu.edu>
2016-05-07 05:30:05 +0000

    finally done with Assignment1

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,8 @@
 class Foobar
-  # ENTER CODE FOR Q2 HERE
+  def initialize(s) # instance method
+  @s = s
+  end
+    def bar(x, arr={})
+      "#{x}#{@s}#{arr[:sat]}"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 post "stringify", to: "pages#stringify" # defines stringify_path form tag
   root 'pages#home'
   put 'age', to: 'pages#person'
-  get 'me' => 'me#about'
+  get 'me' => 'me#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
